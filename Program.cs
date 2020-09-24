94a6eb864f0539d28dc8754325bb0a1e4be86656
AlenBG <57811480+AlenBG@users.noreply.github.com>
2020-09-24 12:39:34 +0300

    fa

--- Program.cs
+++ Program.cs
@@ -1,36 +1,61 @@
-﻿using Microsoft.VisualBasic.CompilerServices;
-using System;
+﻿using System;
 using System.Linq;
 
-namespace _2.__2X2_Squares_in_Matrix
+namespace _3._Maximal_Sum
 {
     class Program
     {
         static void Main(string[] args)
         {
             int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            char[,] matrix = new char[array[0], array[1]];
-            int counter = 0;
+            int[,] matrix = new int[array[0], array[1]];
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                char[] currentRow = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                int[] currentRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
                     matrix[row, col] = currentRow[col];
                 }
             }
-
-            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            int maxSum = 0;
+            int currentSum = 0;
+            int[,] maxSumMatrix = new int[3, 3];
+            int[] maxColRow = new int[2];
+            for (int row = 1; row < matrix.GetLength(0) - 1; row++)
             {
-                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                for (int col = 1; col < matrix.GetLength(1) - 1; col++)
                 {
-                    if(matrix[row,col] == matrix[row,col + 1] && matrix[row, col] == matrix[row + 1, col] && matrix[row, col] == matrix[row + 1, col + 1])
+                    if (currentSum <= maxSum)
                     {
-                        counter++;
+                        currentSum = matrix[row - 1, col - 1] + matrix[row, col - 1] + matrix[row + 1, col - 1] + matrix[row - 1, col] + matrix[row, col] + matrix[row + 1, col] + matrix[row - 1, col + 1] + matrix[row, col + 1] + matrix[row + 1, col + 1];
+                    }
+                    else
+                    {
+                        maxSum = currentSum;
+                        maxColRow[0] = row;
+                        maxColRow[1] = col;
+                        currentSum = 0;
                     }
                 }
             }
-            Console.WriteLine(counter);
+            maxSumMatrix[0, 0] = matrix[maxColRow[0] - 1, maxColRow[1] - 1 - 1];
+            maxSumMatrix[0, 1] = matrix[maxColRow[0], maxColRow[1] - 1 - 1];
+            maxSumMatrix[0, 2] = matrix[maxColRow[0] + 1, maxColRow[1] - 1 - 1];
+            maxSumMatrix[1, 0] = matrix[maxColRow[0] - 1, maxColRow[1] - 1];
+            maxSumMatrix[1, 1] = matrix[maxColRow[0], maxColRow[1] - 1];
+            maxSumMatrix[1, 2] = matrix[maxColRow[0] + 1, maxColRow[1] - 1];
+            maxSumMatrix[2, 0] = matrix[maxColRow[0] - 1, maxColRow[1] + 1 - 1 ];
+            maxSumMatrix[2, 1] = matrix[maxColRow[0], maxColRow[1] + 1 -1 ];
+            maxSumMatrix[2, 2] = matrix[maxColRow[0] + 1, maxColRow[1] + 1 -1 ];
+            Console.WriteLine($"Sum = {maxSum}");
+            for (int row = 0; row < maxSumMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < maxSumMatrix.GetLength(1); col++)
+                {
+                    Console.Write(maxSumMatrix[col,row] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
